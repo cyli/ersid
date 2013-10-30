b93a70920949b26df5a512feb9e163d23c810dd4
cyli <cyli@twistedmatrix.com>
2013-10-30 13:41:37 -0700

    Add support for polling, with a little bit of toy javascript to display changes

--- ersid/rest.py
+++ ersid/rest.py
@@ -1,3 +1,8 @@
+import json
+
+from twisted.internet.defer import Deferred
+from twisted.web.static import File
+
 from klein import Klein
 
 
@@ -6,11 +11,22 @@ class Service(object):
 
     def __init__(self, storage):
         self.storage = storage
+        self.changes_listeners = []
 
     @app.route('/<key>', methods=['POST'])
     def set_key(self, request, key):
-        return self.storage.set(key, request.content.getvalue())
+        listeners, self.changes_listeners = self.changes_listeners, []
 
+        val = request.content.getvalue()
+        d = self.storage.set(key, val)
+
+        def notify(passthru):
+            change = '"{0}" has been set to "{1}"'.format(key, val)
+            for listener in listeners:
+                listener.callback(change)
+            return passthru
+
+        return d.addCallback(notify)
 
     @app.route('/<key>', methods=['GET'])
     def get_key(self, request, key):
@@ -23,6 +39,20 @@ class Service(object):
             return data
 
         return d.addCallback(gotKey)
+
+    @app.route('/_changes', methods=['GET'])
+    def get_changes_feed(self, request):
+        d = Deferred()
+        self.changes_listeners.append(d)
+        return d
+
+    @app.route('/display.html', branch=True)
+    def html(self, request):
+        return File("./public/display.html")
+
+    @app.route('/jquery-1.10.2.min.js', branch=True)
+    def jquery(self, request):
+        return File("./public/jquery-1.10.2.min.js")
 
 
 __all__ = ['Service']
